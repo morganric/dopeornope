9ad2ce464f95365c641f444284f824258229fcc8
morganric <morganric@gmail.com>
2014-10-19 13:21:07 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :votes
 
 
-  get "/posts/:id/dope" => "posts#dope", :as => "dope"
-  get "/posts/:id/nope" => "posts#nope", :as => "nope"
+  post "/posts/:id/dope" => "posts#dope", :as => "dope", via: [:get, :post]
+  post "/posts/:id/nope" => "posts#nope", :as => "nope", via: [:get, :post]
 
   mount Upmin::Engine => '/admin'
   root to: 'posts#index'
